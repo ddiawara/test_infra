8800db21878298b3e62eb559c6924102768e6a41
DIAWARA Daman <d.diawara@bindtech.xyz>
2022-04-18 16:32:57 +0200

    correction fmt

--- main.tf
+++ main.tf
@@ -1,20 +1,7 @@
 terraform {
-  # REQUIRED PROVIDERS
   required_providers {
-#    aws = {
-#      source = "hashicorp/aws"
-#      version = "4.10.0"
-#    }
-#    time = {
-#      source = "hashicorp/time"
-#      version = "0.7.2"
-#    }
-#    null = {
-#      source = "hashicorp/null"
-#      version = "3.1.1"
-#    }
     tls = {
-      source = "hashicorp/tls"
+      source  = "hashicorp/tls"
       version = "3.3.0"
     }
   }
